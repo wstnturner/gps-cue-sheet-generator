e3e976e5ec3de3b7f46c1b065348c9112c43bc5e
kurteous <kurteous@3d96b8ab-5352-e802-4680-9afbca378dfc>
2011-01-13 01:17:18 +0000

    test update

--- trunk/src/cis522/PathFinder.java
+++ trunk/src/cis522/PathFinder.java
@@ -2,6 +2,6 @@ package cis522;
 
 class PathFinder {
     public static void main(String[] args) {
-        System.out.println("Hello PathFinder!"); // Display the string.
+        System.out.println("Hello PathFinder!!!!"); // Display the string.
     }
 }
