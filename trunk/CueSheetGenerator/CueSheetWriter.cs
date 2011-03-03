8d3af453b7fc41bb76fa59665086acfbbba47f45
wstnturner <wstnturner@3d96b8ab-5352-e802-4680-9afbca378dfc>
2011-03-03 06:29:05 +0000

    Forgot to add the files.

---
+++ trunk/CueSheetGenerator/CueSheetWriter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CueSheetGenerator {
+    abstract class CueSheetWriter {
+        protected string _status = "Ok";
+        /// <summary>
+        /// status string
+        /// </summary>
+        public string Status {
+            get { return _status; }
+        }
+
+        protected string getDistanceInUnits(double distance, string units) {
+            switch (units) {
+                case "Meters": return Math.Round(distance, 1).ToString();
+                case "Kilometers": return Math.Round(distance / 1000.0, 1).ToString();
+                case "Miles": return Math.Round(distance
+                    / DirectionsPrinter.METERS_PER_MILE, 1).ToString();
+                default: return null;
+            }
+        }
+
+        public abstract void writeCueSheet(string fileName
+            , List<Address> locs, List<Turn> turns, string units);
+    }
+}
---
+++ trunk/CueSheetGenerator/HtmlWriter.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace CueSheetGenerator {
+    class HtmlWriter : CueSheetWriter {
+        /// <summary>
+        /// write the comma seperated value list of turns out to a file
+        /// </summary>
+        public override void writeCueSheet(string fileName
+            , List<Address> locs, List<Turn> turns, string units) {
+            try {
+                StringBuilder csvFile = new StringBuilder();
+                //case for meters, kilometers, and miles
+                csvFile.Append("Start at " + locs[0].AddressString.Replace(",", "") + "\r\n");
+                csvFile.Append("Interval " + units + ",Total " + units + ",Turn"
+                + ",Degrees,Street,Notes,Latitude,Longitude,Elevation (m)"
+                + ",UTM Zone,Easting,Northing\r\n");
+                for (int i = 0; i < turns.Count; i++) {
+                    csvFile.Append(getDistanceInUnits(turns[i].Distance, units)
+                        + "," + getDistanceInUnits(turns[i].Locs[1].GpxLocation.Distance, units)
+                        + "," + turns[i].TurnDirection
+                        + "," + Math.Round(turns[i].TurnMagnitude)
+                        + "," + turns[i].Locs[2].StreetName + "," + turns[i].Notes
+                        + "," + turns[i].Locs[1].GpxLocation.Lat
+                        + "," + turns[i].Locs[1].GpxLocation.Lon
+                        + "," + turns[i].Locs[1].GpxLocation.Elevation
+                        + "," + turns[i].Locs[1].GpxLocation.Zone
+                        + "," + turns[i].Locs[1].GpxLocation.Easting
+                        + "," + turns[i].Locs[1].GpxLocation.Northing + "\r\n");
+                }
+                csvFile.Append("End at " + locs[locs.Count - 1].AddressString.Replace(",", "")
+                    + "\r\ntotal distance: " + getDistanceInUnits(locs[locs.Count - 1]
+                    .GpxLocation.Distance, units) + " " + units + "\r\n");
+                StreamWriter sr = new StreamWriter(fileName);
+                sr.WriteLine(csvFile.ToString());
+                sr.Close();
+            } catch (Exception e) {
+                _status = e.Message;
+            }
+        }
+    }
+}
